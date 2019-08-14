1926204c70f97cdb62e522ec3da148cf1e297831
Giuseppe Barbato <giuseppe95barbato@gmail.com>
2019-08-14 18:08:38 +0200

    Added Test project providing test for ActorController & QuoteController

---
+++ AldoGiovanniGiacomo.API-Test/Controllers/ActorControllerTest.cs
@@ -0,0 +1,158 @@
+using AldoGiovanniGiacomo.API.Contexts;
+using AldoGiovanniGiacomo.API.Controllers;
+using AldoGiovanniGiacomo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AldoGiovanniGiacomo.API_Test.Controllers
+{
+    public class ActorControllerTest
+    {
+        private readonly AldoGiovanniGiacomoAPIContext _context;
+        private readonly ILogger<ActorController> _logger;
+        public ActorControllerTest()
+        {
+            _context = Initialize.CreateNewTestContext();
+            Initialize.SeedTestDb(_context);
+
+            _logger = Mock.Of<ILogger<ActorController>>();
+        }
+
+
+        [Fact]
+        public async Task GetActors_OkStatusCode_WithEmptyContext()
+        {
+            // Arrange
+            var optionBuilder = new DbContextOptionsBuilder<AldoGiovanniGiacomoAPIContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var exmptyContext = new AldoGiovanniGiacomoAPIContext(optionBuilder.Options);
+            var controller = new ActorController(exmptyContext, _logger);
+
+            // Act
+            var result = await controller.GetActors();
+            var okResult = result as OkObjectResult;
+            var actors = okResult.Value as ICollection<Actor>;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.Equal(0, actors.Count);
+        }
+
+        [Fact]
+        public async Task GetActors_OkStatusCode_WithNOTEmptyContext()
+        {
+            // Arrange
+            var controller = new ActorController(_context, _logger);
+
+            // Act
+            var result = await controller.GetActors();
+            var okResult = result as OkObjectResult;
+            var actors = okResult.Value as ICollection<Actor>;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.Equal(3, actors.Count);
+        }
+
+        [Fact]
+        public async Task GetActor_OkStatusCode_WithValidId()
+        {
+            // Arrange
+            var controller = new ActorController(_context, _logger);
+
+            // Act
+            var result = await controller.GetActor(1);
+            var okResult = result as OkObjectResult;
+            var actor = okResult.Value as Actor;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.Equal("Aldo", actor.Name);
+            Assert.Equal("Baglio", actor.Surname);
+            Assert.Equal("Palermo", actor.BirthPlace);
+            Assert.Equal(new DateTime(1958, 9, 28), actor.Birth);
+        }
+
+        [Fact]
+        public async Task GetActor_NotFoundStatusCode_WithInvalidId()
+        {
+            // Arrange
+            var controller = new ActorController(_context, _logger);
+
+            // Act
+            var result = await controller.GetActor(4);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetActorQuotes_OkStatusCode_WithValidId()
+        {
+            // Arrange
+            var controller = new ActorController(_context, _logger);
+
+            // Act
+            var result = await controller.GetActorQuotes(1);
+            var okResult = result as OkObjectResult;
+            var quotes = okResult.Value as ICollection<Quote>;
+
+            //Assert
+            Assert.Equal(2, quotes.Count);
+        }
+
+        [Fact]
+        public async Task GetActorQuotes_NotFoundStatusCode_WithInvalidId()
+        {
+            // Arrange
+            var controller = new ActorController(_context, _logger);
+
+            // Act
+            var result = await controller.GetActorQuotes(4);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetRandomQuote_OkStatusCode_WithValidId()
+        {
+            // Arrange
+            var controller = new ActorController(_context, _logger);
+
+            // Act
+            var result = await controller.GetRandomQuote(1);
+            var okResult = result as OkObjectResult;
+            var randomQuote = okResult.Value;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.IsType<Quote>(randomQuote);
+        }
+
+        [Fact]
+        public async Task GetRandomQuote_NotFoundStatusCode_WithInvalidId()
+        {
+            // Arrange
+            var controller = new ActorController(_context, _logger);
+
+            // Act
+            var result = await controller.GetActorQuotes(4);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+    }
+}
---
+++ AldoGiovanniGiacomo.API-Test/Controllers/QuoteControllerTest.cs
@@ -0,0 +1,115 @@
+﻿using AldoGiovanniGiacomo.API.Contexts;
+using AldoGiovanniGiacomo.API.Controllers;
+using AldoGiovanniGiacomo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AldoGiovanniGiacomo.API_Test.Controllers
+{
+    public class QuoteControllerTest
+    {
+        private readonly AldoGiovanniGiacomoAPIContext _context;
+        private readonly ILogger<QuoteController> _logger;
+        public QuoteControllerTest()
+        {
+            _context = Initialize.CreateNewTestContext();
+            Initialize.SeedTestDb(_context);
+
+            _logger = Mock.Of<ILogger<QuoteController>>();
+        }
+
+        [Fact]
+        public async Task GetQuotes_OkStatusCode_WithEmptyContext()
+        {
+            // Arrange
+            var optionBuilder = new DbContextOptionsBuilder<AldoGiovanniGiacomoAPIContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var exmptyContext = new AldoGiovanniGiacomoAPIContext(optionBuilder.Options);
+            var controller = new QuoteController(exmptyContext, _logger);
+
+            // Act
+            var result = await controller.GetQuotes();
+            var okResult = result as OkObjectResult;
+            var quotes = okResult.Value as ICollection<Quote>;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.Equal(0, quotes.Count);
+        }
+
+        [Fact]
+        public async Task GetQuotes_OkStatusCode_WithNOTEmptyContext()
+        {
+            // Arrange
+            var controller = new QuoteController(_context, _logger);
+
+            // Act
+            var result = await controller.GetQuotes();
+            var okResult = result as OkObjectResult;
+            var quotes = okResult.Value as ICollection<Quote>;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.Equal(11, quotes.Count);
+        }
+
+        [Fact]
+        public async Task GetQuote_OkStatusCode_WithValidId()
+        {
+            // Arrange
+            var controller = new QuoteController(_context, _logger);
+
+            // Act
+            var result = await controller.GetQuote(1);
+            var okResult = result as OkObjectResult;
+            var quote = okResult.Value as Quote;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.Equal("Aldo Baglio", quote.Actor);
+            Assert.Equal("E voi chi minchia siete?", quote.Content);
+            Assert.Equal("La leggenda di Al, John e Jack", quote.Movie);
+            Assert.Equal(2002, quote.Year);
+        }
+
+        [Fact]
+        public async Task GetQuote_NotFoundStatusCode_WithInvalidId()
+        {
+            // Arrange
+            var controller = new QuoteController(_context, _logger);
+
+            // Act
+            var result = await controller.GetQuote(12);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetRandomQuote_OkStatusCode()
+        {
+            // Arrange
+            var controller = new QuoteController(_context, _logger);
+
+            // Act
+            var result = await controller.GetRandomQuote();
+            var okResult = result as OkObjectResult;
+            var randomQuote = okResult.Value as Quote;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.IsType<Quote>(randomQuote);
+        }
+    }
+}
